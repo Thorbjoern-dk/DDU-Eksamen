6c2f4da0b537a08934d74806363b0aa50f73204d
Thorbjoern-dk <thorbjornmorsing@icloud.com>
2025-04-23 10:06:32 +0200

    yes

--- Assets/LevelFinish.cs
+++ Assets/LevelFinish.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class LevelFinish : MonoBehaviour
 {
+    bool player1IsFinish;
+    bool player2IsFinish;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,23 @@ public class LevelFinish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(player1IsFinish&&player2IsFinish){
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player1")){
+            player1IsFinish = true;
+        } else if(other.CompareTag("Player2")){
+            player2IsFinish = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Player1")){
+            player1IsFinish = false;
+        } else if(other.CompareTag("Player2")){
+            player2IsFinish = false;
+        }
     }
 }
